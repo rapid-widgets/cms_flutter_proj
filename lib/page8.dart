804dae8b759d33c759ed583a7eef54eab35f62de
gautamiyeole <gautami.yeole@pwc.com>
2020-10-16 07:11:30 +0000

    new commit added

--- lib/page8.dart
+++ lib/page8.dart
@@ -103,9 +103,9 @@ class _BoilerPlate extends State<BoilerPlate> {
         
 
         TextParameters textparam0 = new TextParameters(
-          text: 'Testing apk',
+          text: 'Testing codemagic',
           fontSize: 12,
-          textColor:Color(0xff007608),
+          textColor:Color(0xff3d0000),
           fontStyle: FontStyle.normal,
           fontFamily: 'Segoe Ui',
           fontWeight: FontWeight.normal,
@@ -121,24 +121,24 @@ class _BoilerPlate extends State<BoilerPlate> {
                     height: 41,
                     width: 220,
                     child: FlatButton(
-                      child: Text('Testing',
+                      child: Text('Testing codemagic',
                         style: TextStyle(color: Colors.white),),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(21.0),
                       ),
-                      color: Color(0xff007608),
+                      color: Color(0xff0d3d00),
                       onPressed: (){},
                     ),
                   ),
                 ),
               ],"",
-              Text('Codemagic testing', textAlign: TextAlign.center,
+              Text('Codemagic Test', textAlign: TextAlign.center,
               style: TextStyle(fontSize: 16, color: Color(0xff707070)),),
             );
             AlertDialogParameters alertobj21 = new AlertDialogParameters(
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20.0)),
-              backgroundColor: Color(0xffff0000),
+              backgroundColor: Color(0xff2e4900),
               elevation: 30,
               contentTextStyle: TextStyle( fontSize: 18.0, color: Colors.blueGrey),
               height: 254,
