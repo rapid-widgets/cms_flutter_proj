028ec845919677ac7aa675db483595b17a1f5322
gautamiyeole <gautami.yeole@pwc.com>
2020-10-21 06:12:31 +0000

    new commit added

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:rapid_widgets_library/widget-classesUsed.dart';
 import 'package:rapid_widgets_library/custom-appBar.dart';
 import 'package:rapid_widgets_library/custom-drawer.dart';
-import 'package:rapid_widgets_library/custom-image.dart';
-import 'package:rapid_widgets_library/custom-text.dart';
-void main() => runApp(MyApp());
-class MyApp extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-home: BoilerPlate());
-}}
+void main() => runApp(BoilerPlate());
 class BoilerPlate extends StatefulWidget 
 { 
 @override 
@@ -62,57 +54,36 @@ class _BoilerPlate extends State<BoilerPlate> {
             print(nIndex); 
             }
         
-onclick0(String sText) { print(sText); }
-        CustomImageContents ImageObj10 = new CustomImageContents(
-        src:'https://files.readme.io/9bdfde0-network_links0.png',
-         semanticLabel: 'Random Network', 
-         imageType: 'Network', 
-         height: 100, width: 200);
-         
-
-        TextParameters textparam1 = new TextParameters(
-          text: 'hello ',
-          fontSize: 14,
-          textColor:Color(0xffab0000),
-          fontStyle: FontStyle.normal,
-          fontFamily: 'Tulpen One',
-          fontWeight: FontWeight.normal,
-          textAlign: TextAlign.center
-        );
-         
 return MaterialApp(
 debugShowCheckedModeBanner: false,
 home: Scaffold(
-backgroundColor: Color(0xffff0000),
+appBar:customAppBar(appBarIcons,paramobject),
+drawer: CustomDrawer(drawerList,onclickDrawer,user1),
 body: Padding(
-padding: const EdgeInsets.only(top:20.0,left:5.0,right:5.0,bottom:5.0),
+padding: const EdgeInsets.all(20.0),
 child: Padding(
-padding: const EdgeInsets.only(top:10.0,left:5.0,right:5.0,bottom:5.0),
+padding: const EdgeInsets.all(25.0),
 child: SingleChildScrollView(
 child: Column(
 children: <Widget>[
 Padding(
-padding: const EdgeInsets.only(top:65.0,left: 25.0,right: 25.0),
-child: Row(mainAxisAlignment: MainAxisAlignment.center,children: [
-SizedBox(height:80),Expanded(flex: 1,child:CustomImage(data: ImageObj10),),
+padding: const EdgeInsets.all(25.0),
+child: Row(children: [
+Expanded(flex: 1,child:Container()),
+Expanded(flex: 1,child:Container()),
 ],),
 ),
 Padding(
-padding: const EdgeInsets.only(top:50.0,left:25.0,right:25.0,bottom:5.0),
-child: Row(mainAxisAlignment: MainAxisAlignment.center,children: [
-SizedBox(height:65),Expanded(flex: 1,child:customText(textparam1)),
+padding: const EdgeInsets.all(25.0),
+child: Row(children: [
+Expanded(flex: 1,child:Container()),
+Expanded(flex: 1,child:Container()),
 ],),
 ),
 Padding(
-padding: const EdgeInsets.all(15.0),
-child: Row(mainAxisAlignment: MainAxisAlignment.center,children: [
-SizedBox(height:0),Expanded(flex: 1,child:Container()),
-],),
-),
-Padding(
-padding: const EdgeInsets.all(2.0),
-child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
-SizedBox(height:200),Expanded(flex: 1,child:Container()),SizedBox(width:30),
+padding: const EdgeInsets.all(25.0),
+child: Row(children: [
+Expanded(flex: 1,child:Container()),
 Expanded(flex: 1,child:Container()),
  ],),
 ),
