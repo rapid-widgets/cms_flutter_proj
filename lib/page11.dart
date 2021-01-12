60e9bf16b277ce70ec43449116d1670b0dc193e0
gautami-yeole <gautamiyeole@gmail.com>
2021-01-12 18:16:09 +0000

    new commit added

--- lib/page11.dart
+++ lib/page11.dart
@@ -103,7 +103,7 @@ class _BoilerPlate extends State<BoilerPlate> {
         
 
         TextParameters textparam0 = new TextParameters(
-          text: 'Thankyou!',
+          text: 'Order Recieved',
           fontSize: 16,
           textColor:Color(0xff000000),
           fontStyle: FontStyle.normal,
@@ -121,24 +121,24 @@ class _BoilerPlate extends State<BoilerPlate> {
                     height: 41,
                     width: 220,
                     child: FlatButton(
-                      child: Text('OK',
+                      child: Text('Ok',
                         style: TextStyle(color: Colors.white),),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(21.0),
                       ),
-                      color: Color(0xfff6f2f2),
+                      color: Color(0xffffffff),
                       onPressed: (){},
                     ),
                   ),
                 ),
               ],"",
-              Text('Order Ready!', textAlign: TextAlign.center,
+              Text('Thankyou', textAlign: TextAlign.center,
               style: TextStyle(fontSize: 16, color: Color(0xff707070)),),
             );
             AlertDialogParameters alertobj21 = new AlertDialogParameters(
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20.0)),
-              backgroundColor: Color(0xfff8f7f7),
+              backgroundColor: Color(0xff000000),
               elevation: 30,
               contentTextStyle: TextStyle( fontSize: 18.0, color: Colors.blueGrey),
               height: 254,
@@ -153,7 +153,7 @@ return MaterialApp(
 debugShowCheckedModeBanner: false,
 home: Scaffold( 
 appBar:customAppBar(appBarIcons,paramobject),
-backgroundColor: Color(0xffffffff),
+backgroundColor: Colors.white,
 body: Padding(
 padding: const EdgeInsets.all(0.0),
 child: Padding(
