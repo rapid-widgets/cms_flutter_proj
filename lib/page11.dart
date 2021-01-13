eff83b56176672236f828090bb9bace68972be5e
gautami-yeole <gautamiyeole@gmail.com>
2021-01-13 02:42:06 +0000

    new commit added

--- lib/page11.dart
+++ lib/page11.dart
@@ -105,7 +105,7 @@ class _BoilerPlate extends State<BoilerPlate> {
         TextParameters textparam0 = new TextParameters(
           text: 'ThankYou!',
           fontSize: 16,
-          textColor:Color(0xff000000),
+          textColor:Color(0xfff3efef),
           fontStyle: FontStyle.normal,
           fontFamily: 'Roboto',
           fontWeight: FontWeight.normal,
@@ -138,7 +138,7 @@ class _BoilerPlate extends State<BoilerPlate> {
             AlertDialogParameters alertobj21 = new AlertDialogParameters(
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20.0)),
-              backgroundColor: Color(0xfff1f1f1),
+              backgroundColor: Color(0xff000000),
               elevation: 30,
               contentTextStyle: TextStyle( fontSize: 18.0, color: Colors.blueGrey),
               height: 254,
