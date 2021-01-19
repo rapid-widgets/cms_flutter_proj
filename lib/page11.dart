effe681b4e51a5f291b19a8bd213f945730e29af
gautami-yeole <gautamiyeole@gmail.com>
2021-01-19 12:27:59 +0000

    new commit added

--- lib/page11.dart
+++ lib/page11.dart
@@ -153,7 +153,7 @@ return MaterialApp(
 debugShowCheckedModeBanner: false,
 home: Scaffold( 
 appBar:customAppBar(appBarIcons,paramobject),
-backgroundColor: Color(0xffffffff),
+backgroundColor: Color(0xffd8caca),
 body: Padding(
 padding: const EdgeInsets.all(0.0),
 child: Padding(
